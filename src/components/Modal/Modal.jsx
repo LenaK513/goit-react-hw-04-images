754fc698f898bccda17e28dc635d861df71c52fe
Olena <kryvtsovaolena@gmail.com>
2022-11-16 14:06:27 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 export function Modal({ onClick, largeImageURL }) {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      console.log(event.code);
+      if (event.code === 'Escape') {
+        onClick();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClick]);
 
-  
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      onClick();
-    }
-  };
-
-  handleBdClick = event => {
+  const handleBdClick = event => {
     if (event.currentTarget === event.target) {
       onClick();
     }
   };
 
   return (
-    <Overlay onClick={this.handleBdClick}>
+    <Overlay onClick={handleBdClick}>
       <ModalWindow>
         <img src={largeImageURL} alt="" />
       </ModalWindow>
@@ -59,8 +64,8 @@ export function Modal({ onClick, largeImageURL }) {
 //   }
 // }
 
-// Modal.propTypes = {
-//   largeImageURL: PropTypes.string.isRequired,
-//   handleBdClick: PropTypes.func,
-//   handleKeyDown: PropTypes.func,
-// };
+Modal.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+  handleBdClick: PropTypes.func,
+  handleKeyDown: PropTypes.func,
+};
